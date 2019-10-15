7cc061d80a12d244fc9c9e4aa79dcd5764998937
Sougata Khan <sougat818@gmail.com>
2019-10-15 17:07:06 +1100

    ProducerDemoWithCallback

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Steps to Run the Tutorial
+# Steps to Run the Tutorial Demo code
 
 ## Start docker
 
@@ -13,6 +13,10 @@ docker exec -ti kafka-1 bash
 kafka-console-consumer --bootstrap-server 127.0.1:9092 --topic first_topic --group application
 ```
 
-## Run tutorial1 
+## Run ProducerDemo 
 
 This will send "hello world" messages to kafka, which will appear in the console consumer screen
+
+## Run ProducerDemoWithCallback
+
+This will send 10 numbered "hello world" messages to kafka, Also prints detailed logs 
--- src/test/java/com/github/sougat818/kafka/tutorial1/ProducerDemoWithDemo.java
+++ src/test/java/com/github/sougat818/kafka/tutorial1/ProducerDemoWithDemo.java
@@ -4,10 +4,15 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
-public class ProducerDemo {
+
+public class ProducerDemoWithDemo {
+
+    private final static Logger logger = LoggerFactory.getLogger(ProducerDemoWithDemo.class);
 
     public static void main(String[] args) {
         // create Producer properties
@@ -19,11 +24,19 @@ public class ProducerDemo {
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-        // create producer record
-        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "hello world");
-
-        //send data
-        producer.send(record);
+        for (int i = 0; i < 10; i++) {
+            // create producer record
+            ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "hello world " + i);
+            logger.info("test");
+            //send data
+            producer.send(record, (recordMetadata, e) -> {
+                if (e != null) {
+                    logger.error("Exception while sending message", e);
+                } else {
+                    logger.info("Message sent to Offset: {} Partition {} Topic {} TimeStamp {}", recordMetadata.offset(), recordMetadata.partition(), recordMetadata.topic(), recordMetadata.timestamp());
+                }
+            });
+        }
 
         //flush and close producer
         producer.flush();
